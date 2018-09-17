784159e36a87b5238048a4b42898652af7ae25a1
Allan <allansachs@icloud.com>
2018-09-17 02:57:20 -0400

    Update footer styles

--- client/src/components/landing/LandingInfo.js
+++ client/src/components/landing/LandingInfo.js
@@ -13,12 +13,13 @@ class LandingInfo extends Component {
             <i className="fa fa-bolt" aria-hidden="true" />
           </div>
           <div className="info-subsection-description">
-            Jytr is a JavaScript code challenge game made up of forty questions
-            in total cut from the same cloth as FizzBuzz.
+            A code challenge game made up of forty FizzBuzzy questions. For more
+            information on this project, check out the&nbsp;
+            <a href="https://github.com/allansachsambia/jytr">repo</a>.
           </div>
         </div>
 
-        <div className="info-subsection">
+        {/* <div className="info-subsection">
           <div className="info-subsection-title">Project Information</div>
 
           <div>
@@ -26,11 +27,9 @@ class LandingInfo extends Component {
           </div>
 
           <div className="info-subsection-description">
-            For more information on the current state of this project, it&apos;s
-            issues, to make recommendations or to help, check out the&nbsp;
-            <a href="https://github.com/allansachsambia/jytr"> repo</a>.
+
           </div>
-        </div>
+        </div> */}
       </div>
     );
   }
